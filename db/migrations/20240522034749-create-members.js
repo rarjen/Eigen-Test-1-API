58629b137aef992ae59b6593333e10221ce86b90
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-22 15:36:14 +0700

    update 20240522034749-create-members.js

--- db/migrations/20240522034749-create-members.js
+++ db/migrations/20240522034749-create-members.js
@@ -1,30 +1,30 @@
-'use strict';
+"use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Members', {
+    await queryInterface.createTable("Members", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       code: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Members');
-  }
+    await queryInterface.dropTable("Members");
+  },
 };
