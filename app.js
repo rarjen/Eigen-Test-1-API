decad419127d3af2cffd7794422889e911955e73
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-23 08:43:07 +0700

    update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const ApiError = require("./helpers/errorHandler");
 const http = require("http");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 const { PORT } = process.env;
 
 const app = express();
@@ -17,6 +20,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1", router);
 
 app.use((req, res, next) => {
