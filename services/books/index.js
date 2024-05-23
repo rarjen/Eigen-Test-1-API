784a4d5bf80aa98df25ce6ac4633e261c3b2e5a5
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-23 07:11:05 +0700

    update services/books/index.js

--- services/books/index.js
+++ services/books/index.js
@@ -2,7 +2,9 @@ const { Books } = require("../../models");
 const { Op } = require("sequelize");
 
 const getAllAvailableBook = async (req) => {
-  const result = await Books.findAll({ where: { stock: { [Op.gt]: 0 } } });
+  const result = await Books.findAll({
+    where: { stock: { [Op.gt]: 0 } },
+  });
 
   return result;
 };
