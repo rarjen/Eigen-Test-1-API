2cf47ed2854e73490a154962ce87374b55ebfb87
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-23 07:10:50 +0700

    update members.js

--- models/members.js
+++ models/members.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Members.hasMany(models.Transactions, {
-        foreignKey: "member_code",
+        foreignKey: "member_id",
         as: "transactions",
       });
     }
