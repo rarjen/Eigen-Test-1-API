2a69154de402f347f3d78338a7f0a0ea75e28d86
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-23 07:10:46 +0700

    update books.js

--- models/books.js
+++ models/books.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Books.hasMany(models.Transactions, {
-        foreignKey: "book_code",
+        foreignKey: "book_id",
         as: "transactions",
       });
     }
