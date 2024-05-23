1d3a31ff7a250969290f6b0b734c9951f18351e5
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-23 08:43:26 +0700

    update transaction.js

--- routes/transaction.js
+++ routes/transaction.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const transaction = require("../controllers/transaction");
 
 router.post("/", transaction.create);
+router.patch("/:id", transaction.update);
 
 module.exports = router;
